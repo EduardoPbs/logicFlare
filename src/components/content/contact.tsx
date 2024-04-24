d65972f1312d76dfc98a6e896fb1f984087d0db6
Eduardo Soares <eduardopbs234@outlook.com>
2024-04-24 10:45:17 -0300

    fix: let same responsive behavior

--- src/components/content/contact.tsx
+++ src/components/content/contact.tsx
@@ -13,15 +13,15 @@ export function Contact(props: IContactProps) {
             <h2 className={contentTitleStylePri}>
                 Contato
             </h2>
-            <div className='flex items-center px-4 justify-center gap-12 py-6 max-w-full flex-wrap'>
-                <div className='flex flex-col sm:flex-row items-center sm:items-start gap-6 md:w-[705px] text-xl text-justify font-semibold'>
-                    <h3 className='max-w-[450px]'>
-                        Envie um email para o email ao lado com informações de seu projeto.
-                        Vamos analisar e retornaremos o contato para discutir detalhes e fornecer um orçamento!
+            <div className='flex items-center justify-center gap-12 py-6 max-w-full flex-wrap'>
+                <div className='flex flex-col items-center justify-center gap-6 md:max-w-[705px] text-xl text-justify font-semibold'>
+                    <h3 className='px-4'>
+                        Envie-nos um e-mail para o endereço abaixo com informações de seu projeto.
+                        Iremos analisar e retornar o contato para discutir detalhes e fornecer um orçamento!
                     </h3>
-                    <p className='flex flex-col items-center'>
-                        Email:
-                        <span className='font-bold text-3xl bg-main-color text-main-white px-2 rounded-lg select-text'>
+                    <p className='flex flex-col items-center select-none'>
+                        E-mail:
+                        <span className='font-bold text-3xl bg-main-color text-main-white px-1 py-1 rounded-md select-text'>
                             logicflare3@gmail.com
                         </span>
                     </p>
